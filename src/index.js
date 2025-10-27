09d112536dd7eb5e8599826ef038fc27c3baa802
ibrahim çıngı <ibrahimcingi@ibrahim-Dizustu-Bilgisayar.local>
2025-10-27 22:05:32 +0300

    connectDB function used

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { connectDB } from "./db/mongo.js";
 
 dotenv.config();
 
@@ -8,10 +9,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+connectDB()
+
 
 app.get("/", (req, res) => {
   res.send("Otonom Blog Servisi Çalışıyor 🚀");
+ 
 });
+
+app.post("/publish",async (req,res)=>{
+  const postData = {
+    title: "Test Postu - API Üzerinden Gönderildi",
+    content: "Bu yazı Node.js servisi üzerinden WordPress REST API'ye gönderildi.",
+    status: "publish"
+  };
+  
+  try {
+    const response = await fetch("https://tamirbilgi.online/wp-json/wp/v2/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization":
+          "Basic " + Buffer.from(`${process.env.WP_USER}:${process.env.WP_APP_PASS}`).toString("base64"),
+      },
+      body: JSON.stringify(postData),
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Post atılırken hata:", error);
+    res.status(500).json({ error: "Post gönderilemedi" });
+  }
+
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Sunucu ${PORT} portunda çalışıyor`));
