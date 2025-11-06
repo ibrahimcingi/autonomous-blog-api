0556ff4a378656637a9c9e19eb46687e47932136
ibrahim çıngı <ibrahimcingi@ibrahim-Dizustu-Bilgisayar.local>
2025-11-06 16:48:33 +0300

    logout function fixed

--- src/auth/authentication.js
+++ src/auth/authentication.js
@@ -87,7 +87,7 @@ Authrouter.post('/logout',(req,res)=>{
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-    maxAge: cookieAge,
+    //maxAge: cookieAge,
   })
   return res.status(200).json({ message: 'Logout successful' })
 })
